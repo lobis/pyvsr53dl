469e8421fda0f5758e0ed86fa69e51d2559caf27
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-02-21 11:55:12 +0100

    add version

--- src/vsr53/__init__.py
+++ src/vsr53/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from vsr53._version import __version__, __version_tuple__
 from vsr53.vsr53 import VSR53DL, VSR53USB
 
-__all__ = ["VSR53USB", "VSR53DL"]
+__all__ = ["VSR53DL", "VSR53USB", "__version__", "__version_tuple__"]
