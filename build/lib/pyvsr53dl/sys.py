0567ec9e15454d34e5df3eb24b8aa77c2e3d0b1e
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-02-21 11:20:26 +0100

    modern packaging

--- build/lib/pyvsr53dl/sys.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-if platform.system() == 'Darwin':
-    dev_tty = '/dev/cu.usbserial-FT4VOTGK'
-elif platform.system() == 'Windows':
-    dev_tty = 'COM6'
-elif platform.system() == 'Linux':
-    dev_tty = '/dev/ttyUSB0'
